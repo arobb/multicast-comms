f271579f55e6e74df5d7719afb4aac396752adc4
Andy Robb <andy@andyrobb.com>
2016-10-23 18:34:54 -0700

    Converted receive-put-apc to a daemon

--- receive-put-apc.py
+++ receive-put-apc.py
@@ -11,127 +11,155 @@ from mListener import mListenerThread
 from mMessage import mMessage
 from mApcMessage import mApcMessage
 import urllib2
+from daemon import Daemon
 
 
-# Absolute directory of the execution script
-# Allows for finding config file even when the script is executed from
-# another location
-dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+class MulticastReceiver(Daemon):
 
+    def setConfig(self, config):
+        self.config = config
 
-# Gracefully handle exit requests
-# http://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
-def signal_handler(signal, frame):
-    sys.exit(0)
+    def run(self):
+        config = self.config
 
-signal.signal(signal.SIGINT, signal_handler)
+        # Create an instance of the multicast listener
+        mlistener = mListenerThread(config)
 
-
-# Process arguments passed at the command line
-parser = argparse.ArgumentParser(usage='%(prog)s [options]', description='Broadcast data to multicast audience.')
-parser.add_argument('-c', '--config-file', required=False, default=dir+'/config.ini', nargs='?', dest='config', help='Path to config file')
-parser.add_argument('-g', '--multicast-group', required=False, nargs='?', dest='group', help='Multicast group/address')
-parser.add_argument('-k', '--shared-key', required=False, nargs='?', dest='key', help='Shared key. Overrides key from config file')
-parser.add_argument('-p', '--multicast-port', required=False, nargs='?', dest='port', help='UDP port to use')
-parser.add_argument('-v', '--debug', required=False, action='store_true', dest='debug', help='Debug mode')
-args, unk = parser.parse_known_args()
-
-
-# Process the config file
-config_parser = ConfigParser.SafeConfigParser()
-config_parser.read(args.config)
-
-
-# Generate a configuration object
-config = {}
-config['q'] = Queue()
-config['ANY'] = "0.0.0.0"
-config['DEBUG'] = args.debug
-config['MCAST_GRP'] = config_parser.get('Network', 'multicast_group') if args.group is None else args.group
-config['MCAST_PORT'] = config_parser.getint('Network', 'multicast_port') if args.port is None else int(args.port)
-config['SHARED_KEY'] = config_parser.get('Security', 'shared_key', 1) if args.key is None else args.key
-
-
-# Create an instance of the multicast listener
-mlistener = mListenerThread(config)
-
-
-while threading.active_count() > 0:
-
-    try:
-        # Grab a message from the queue if one is available
-        m = config['q'].get_nowait()
-        apc = mApcMessage(m)
-
-        influxhostport="raspberrypi.local:8086"
-        influxdatabase="power"
-
-        url     = "http://{0}/write?db={1}&precision=s".format(influxhostport, influxdatabase)
-
-        epoch   = int(time.time())
-        upsname = apc.getFieldValue('UPSNAME').replace(' ', '_')
-        model   = apc.getFieldValue('MODEL').replace(' ', '_')
-
-        measurements = { \
-              'power_line_volts': 'LINEV' \
-            , 'power_load_pct': 'LOADPCT' \
-            , 'power_battery_volts': 'BATTV' \
-            , 'power_minutes_left': 'TIMELEFT' \
-            , 'power_battery_charge_pct': 'BCHARGE' \
-            , 'power_seconds_on_battery': 'TONBATT' \
-        }
-
-        for table, field in measurements.iteritems():
-            submission="{0},name={1},model={2} value={3} {4}".format(\
-                  table \
-                , upsname \
-                , model \
-                , apc.getFieldValue(field) \
-                , epoch \
-            )
+        while threading.active_count() > 0:
 
             try:
-                req    = urllib2.Request(url, headers={'Content-Type': 'text/plain'})
-                result = urllib2.urlopen(url=req, data=submission, timeout=5)
+                # Grab a message from the queue if one is available
+                m = config['q'].get_nowait()
+                apc = mApcMessage(m)
+
+                influxhostport="raspberrypi.local:8086"
+                influxdatabase="power"
+
+                url     = "http://{0}/write?db={1}&precision=s".format(influxhostport, influxdatabase)
+
+                epoch   = int(time.time())
+                upsname = apc.getFieldValue('UPSNAME').replace(' ', '_')
+                model   = apc.getFieldValue('MODEL').replace(' ', '_')
+
+                measurements = { \
+                      'power_line_volts': 'LINEV' \
+                    , 'power_load_pct': 'LOADPCT' \
+                    , 'power_battery_volts': 'BATTV' \
+                    , 'power_minutes_left': 'TIMELEFT' \
+                    , 'power_battery_charge_pct': 'BCHARGE' \
+                    , 'power_seconds_on_battery': 'TONBATT' \
+                }
+
+                for table, field in measurements.iteritems():
+                    submission="{0},name={1},model={2} value={3} {4}".format(\
+                          table \
+                        , upsname \
+                        , model \
+                        , apc.getFieldValue(field) \
+                        , epoch \
+                    )
+
+                    try:
+                        req    = urllib2.Request(url, headers={'Content-Type': 'text/plain'})
+                        result = urllib2.urlopen(url=req, data=submission, timeout=5)
+
+                    except Exception as e:
+                        print "Attempted to send '{0}'".format(submission)
+                        print e
+
+
+                # Handle status separately because it's a non-numeric value
+                table="power_status"
+                field="STATUS"
+                submission="{0},name={1},model={2} value=\"{3}\" {4}".format(\
+                      table \
+                    , upsname \
+                    , model \
+                    , apc.getFieldValue(field).lower() \
+                    , epoch \
+                )
+
+                try:
+                    req    = urllib2.Request(url, headers={'Content-Type': 'text/plain'})
+                    result = urllib2.urlopen(url=req, data=submission, timeout=5)
+
+                except (urllib2.URLError, urllib2.HTTPError) as e:
+                    print "Attempted to send '{0}'".format(submission)
+                    print e.reason
+
+                # BATTV, MODEL, TIMELEFT, BCHARGE, TONBATT, LINEV, STATUS, LOADPCT, UPSNAME
+                # power_status,name=upsname,model=model value=STATUS epoch
+                # power_load,upsname,model
+                # power_line_volts,upsname,model
+                # power_battery_volts,upsname,model
+                # power_timeleft,upsname,model
+                # power_battery_charge,upsname,model
+                # power_time_on_battery,upsname,model
+
+                config['q'].task_done()
 
             except Exception as e:
-                print "Attempted to send '{0}'".format(submission)
-                print e
+                # print e
+                # If no messages are available, just wait a moment and try again
+                time.sleep(0.5)
 
 
-        # Handle status separately because it's a non-numeric value
-        table="power_status"
-        field="STATUS"
-        submission="{0},name={1},model={2} value=\"{3}\" {4}".format(\
-              table \
-            , upsname \
-            , model \
-            , apc.getFieldValue(field).lower() \
-            , epoch \
-        )
+if __name__ == '__main__':
 
-        try:
-            req    = urllib2.Request(url, headers={'Content-Type': 'text/plain'})
-            result = urllib2.urlopen(url=req, data=submission, timeout=5)
-
-        except (urllib2.URLError, urllib2.HTTPError) as e:
-            print "Attempted to send '{0}'".format(submission)
-            print e.reason
-
-        # BATTV, MODEL, TIMELEFT, BCHARGE, TONBATT, LINEV, STATUS, LOADPCT, UPSNAME
-        # power_status,name=upsname,model=model value=STATUS epoch
-        # power_load,upsname,model
-        # power_line_volts,upsname,model
-        # power_battery_volts,upsname,model
-        # power_timeleft,upsname,model
-        # power_battery_charge,upsname,model
-        # power_time_on_battery,upsname,model
+    # Absolute directory of the execution script
+    # Allows for finding config file even when the script is executed from
+    # another location
+    dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 
 
+    # Gracefully handle exit requests
+    # http://stackoverflow.com/questions/1112343/how-do-i-capture-sigint-in-python
+    def signal_handler(signal, frame):
+        sys.exit(0)
 
-        config['q'].task_done()
+    signal.signal(signal.SIGINT, signal_handler)
 
-    except Exception as e:
-        # print e
-        # If no messages are available, just wait a moment and try again
-        time.sleep(0.5)
+
+    # Daemon instance
+    daemon = MulticastReceiver('/tmp/MulticastReceiverApcWriter.pid', stdin='/dev/null', stdout='/dev/stdout', stderr='/dev/stderr')
+
+
+    # Process arguments passed at the command line
+    parser = argparse.ArgumentParser(usage='%(prog)s [options]', description='Broadcast data to multicast audience.')
+    parser.add_argument('-c', '--config-file', required=False, default=dir+'/config.ini', nargs='?', dest='config', help='Path to config file')
+    parser.add_argument('-g', '--multicast-group', required=False, nargs='?', dest='group', help='Multicast group/address')
+    parser.add_argument('-k', '--shared-key', required=False, nargs='?', dest='key', help='Shared key. Overrides key from config file')
+    parser.add_argument('-p', '--multicast-port', required=False, nargs='?', dest='port', help='UDP port to use')
+    parser.add_argument('-v', '--debug', required=False, action='store_true', dest='debug', help='Debug mode')
+
+    sp = parser.add_subparsers()
+    sp_start   = sp.add_parser('start', help='Start Daemon mode')
+    sp_start.set_defaults(func=daemon.start)
+
+    sp_stop    = sp.add_parser('stop', help='Stop Daemon mode')
+    sp_stop.set_defaults(func=daemon.stop)
+
+    sp_restart = sp.add_parser('restart', help='Restart Daemon mode')
+    sp_restart.set_defaults(func=daemon.restart)
+
+    args, unk = parser.parse_known_args()
+
+
+    # Process the config file
+    config_parser = ConfigParser.SafeConfigParser()
+    config_parser.read(args.config)
+
+
+    # Generate a configuration object
+    config = {}
+    config['q'] = Queue()
+    config['ANY'] = "0.0.0.0"
+    config['DEBUG'] = args.debug
+    config['MCAST_GRP'] = config_parser.get('Network', 'multicast_group') if args.group is None else args.group
+    config['MCAST_PORT'] = config_parser.getint('Network', 'multicast_port') if args.port is None else int(args.port)
+    config['SHARED_KEY'] = config_parser.get('Security', 'shared_key', 1) if args.key is None else args.key
+
+
+    # Trigger the requested function
+    daemon.setConfig(config)
+    args.func()
